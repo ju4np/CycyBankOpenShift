4bdcb28da2c8964dfe75bfb52c6888022d723a8d
ju4np <juama31@gmail.com>
2015-02-05 09:46:17 +0100

    Actualizacion 05/02/2015: 	- Actualizados SQL para la migracion

--- src/java/com/fpmislata/banco/persistencia/migration/V04__UpdateCuentas.sql
+++ src/java/com/fpmislata/banco/persistencia/migration/V04__UpdateCuentas.sql
@@ -2,3 +2,5 @@ ALTER TABLE `cuenta`
 	CHANGE COLUMN `sucursalBancaria` `sucursalBancaria` INT NOT NULL AFTER `fechaCreacion`;
 ALTER TABLE `cuenta`
 	ADD COLUMN `cuentaBancaria` INT(24) ZEROFILL NOT NULL AFTER `sucursalBancaria`;
+ALTER TABLE `sucursal`
+	CHANGE COLUMN `codigoSucursal` `codigoSucursal` INT(3) NOT NULL DEFAULT '0' AFTER `localizacion`;
