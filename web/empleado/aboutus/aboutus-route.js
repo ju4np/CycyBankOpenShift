42c80ca417adf3480640692203698d66a6496736
JuanPe <crazycryzocorp@gmail.com>
2015-02-24 00:02:44 +0100

    Actualizacion About Us Proyecto Ingles

---
+++ web/empleado/aboutus/aboutus-route.js
@@ -0,0 +1,9 @@
+app.config(['$routeProvider', function($routeProvider) {
+
+        $routeProvider.when('/aboutus', {
+            templateUrl: "aboutus/aboutus.html",
+            controller: ""
+        });
+        
+    }]);
+
